94fa30ee14d4ea748a58326c5d2ea2380def9023
BloodWolv3 <73177242+BloodWolv3@users.noreply.github.com>
2020-10-20 15:21:16 +0200

    Add files via upload

--- Account_File.py
+++ Account_File.py
@@ -1,8 +1,8 @@
 class Account:
-    def __init__(self, Account_Name, Account_Email, Account_Password):
+    def __init__(self, account_name, account_email, account_password):
         self.account_name = account_name
-        self.account_email = Account_email
-        self.account_password = Account_password
+        self.account_email = account_email
+        self.account_password = account_password
 
 
 #Account Verlinkung zu Social Media 
