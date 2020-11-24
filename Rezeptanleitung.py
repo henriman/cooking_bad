0dc1c1584237aeb596718991f7964bf868a190a9
BloodWolv3 <73177242+BloodWolv3@users.noreply.github.com>
2020-11-24 14:35:24 +0100

    Add files via upload

---
+++ Rezeptanleitung.py
@@ -0,0 +1,8 @@
+class Instruction:
+    def __init__(self, Rezeptanleitung)
+        self.Rezeptanleitung = Rezeptanleitung
+
+        # keine Ahnung wie man das schön machen kann
+        # Dies soll die Möglichkeit geben eine Rezeptanleitung zu schreiben
+        # Bitte überprüf das mal Henri oder ist das zu simpel 
+        
