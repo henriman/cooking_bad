aabe8cd2ba0028473444b43074e22b943ad9db10
BloodWolv3 <73177242+BloodWolv3@users.noreply.github.com>
2020-11-24 15:08:39 +0100

    Add files via upload

--- Rezeptanleitung.py
+++ Rezeptanleitung.py
@@ -1,8 +1,26 @@
 class Instruction:
-    def __init__(self, Rezeptanleitung)
+    def __init__(self, |Rezeptname|, |Rezeptanleitung|)
         self.Rezeptanleitung = Rezeptanleitung
+        self.Rezeptname = Rezeptname
 
-        # keine Ahnung wie man das schön machen kann
-        # Dies soll die Möglichkeit geben eine Rezeptanleitung zu schreiben
-        # Bitte überprüf das mal Henri oder ist das zu simpel 
+        Rezeptname = input("Geben sie hier ihren Rezeptnamen an")
+        Rezeptanleitung = input("Geben sie hier ihre Rezeptanleitung in Schritten an")
+        if Rezeptanleitung #mach eine neue Tabellenspalte und lass mich weiter schreiben
+
+        elif Rezeptanleitung #fertig beende es mit done und die Rezeptanleitung wird weiter verwertet
+
+        #Ich habe das mit Tabellen noch nicht so ganz verstanden wo man das hinschreiben soll
+        #Vielleicht kannst du mal mit einem Kommentar mir Bescheid geben, ob das so funktioniert 
+        
+        
+        table = [
+            |Rezeptname|,
+            |Rezeptanleitungx|,
+            |Rezeptanleitungx|,
+            ]
+
+        
+        #So habe ich das im Internet gefunden, habe immer noch nicht ganz verstanden wie das jetzt genau auf unsere
+        #Liste passt, warscheinlich ist das sogar komplett falsch xD
+        #Sorry das ich dich so nerve xD
         
